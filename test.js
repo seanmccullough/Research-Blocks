640a1e176733b0dd50d16f4ea7da0b1de570688d
Pranav Menon <menon1@purdue.edu>
2013-09-21 09:55:03 -0400

    Added parse*.js, updated context menus

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
-/*Parse.initialize("5qbsu1eWpUXnoeb2JnL7ZcxqPXiajZOGsSkyk7fB", "YYOUQ0x2decE180Il4Y5mZGUIzAcWR7VTGsYsZvb");
+
+
+Parse.initialize("5qbsu1eWpUXnoeb2JnL7ZcxqPXiajZOGsSkyk7fB", "YYOUQ0x2decE180Il4Y5mZGUIzAcWR7VTGsYsZvb");
 
 var TestObject = Parse.Object.extend("TestObject");
 var testObject = new TestObject();
@@ -6,30 +8,49 @@ testObject.save({foo: "bar"}, {
   success: function(object) {
     alert("yay! it worked");
   }
-});*/
+});
+
 // The "onclick" functions, will do different things depending on the type of item selected
 
 // page selected
-function testOnClick_page(info)
+function testOnClick(info)
 {
-	alert(info.menuItemId + " was clicked, " + "page");
+	console.log(info.menuItemId + " was clicked");
+	console.log("page URL is :  " + info.pageUrl);
+	console.log("link URL is :  " + info.linkUrl);
+	console.log("text is :  " + info.selectionText);
+	console.log("mediaType is :  " + info.mediaType);
 	
-	console.log("URL is :  " + info.pageUrl);
+	if(info.mediaType=="image")
+		console.log("This is an image\n\n");
+	else
+	if(info.selectionText!=undefined)
+		console.log("This is a selecion text\n\n");
+	else
+	if(info.linkUrl!=undefined)
+		console.log("This is a link\n\n");
+	else
+	if(info.pageUrl!=undefined)
+		console.log("This is a page\n\n");
 }
 
 // link selected
-function testOnClick_link(info)
+/*function testOnClick_link(info)
 {
-	alert(info.menuItemId + " was clicked, " + "link");
-	console.log("URL is :  " + info.linkUrl);
+	console.log(info.menuItemId + " was clicked, " + "link");
+	console.log("page URL is :  " + info.pageUrl);
+	console.log("link URL is :  " + info.linkUrl);
+	console.log("text is :  " + info.selectionText);
 }
 
 // selection selected
 function testOnClick_selection(info)
 {
-	alert(info.menuItemId + " was clicked, " + "selection");
+	console.log(info.menuItemId + " was clicked, " + "selection");
+	console.log("page URL is :  " + info.pageUrl);
+	console.log("link URL is :  " + info.linkUrl);
 	console.log("text is :  " + info.selectionText);
-}
+}*/
 
 function inputBlockDetails()
 {
@@ -54,26 +75,26 @@ var fillBlock = chrome.contextMenus.create({"title": "Add to Block", "contexts":
 
 // Adding test blocks
 
-var testBlock1_page = chrome.contextMenus.create(
-  {"title": "Data Structures", "parentId": fillBlock, "onclick": testOnClick_page, "contexts":["page"], "id": "Block1_page"});
+var testBlock1 = chrome.contextMenus.create(
+  {"title": "Data Structures", "parentId": fillBlock, "onclick": testOnClick, "contexts":["page", "link", "image", "selection"], "id": "Block1_page"});
  
-var testBlock1_link = chrome.contextMenus.create(
+/*var testBlock1_link = chrome.contextMenus.create(
   {"title": "Data Structures", "parentId": fillBlock, "onclick": testOnClick_link, "contexts":["link", "image"], "id": "Block1_link"});
   
 var testBlock1_selection = chrome.contextMenus.create(
   {"title": "Data Structures", "parentId": fillBlock, "onclick": testOnClick_selection, "contexts":["selection"], "id": "Block1_selection"});
+*/
 
 
+var testBlock2 = chrome.contextMenus.create(
+  {"title": "Algorithms", "parentId": fillBlock, "onclick": testOnClick, "contexts":["page", "link", "image", "selection"], "id": "Block2_page"});
 
-var testBlock2_page = chrome.contextMenus.create(
-  {"title": "Algorithms", "parentId": fillBlock, "onclick": testOnClick_page, "contexts":["page"], "id": "Block2_page"});
-
-var testBlock2_link = chrome.contextMenus.create(
+/*var testBlock2_link = chrome.contextMenus.create(
   {"title": "Algorithms", "parentId": fillBlock, "onclick": testOnClick_link, "contexts":["link", "image"], "id": "Block2_link"});
   
 var testBlock2_selection = chrome.contextMenus.create(
   {"title": "Algorithms", "parentId": fillBlock, "onclick": testOnClick_selection, "contexts":["selection"], "id": "Block2_selection"});
-  
+  */
 //console.log("parent:" + parent + " child1:" + child1 + " child2:" + child2);
 
 
