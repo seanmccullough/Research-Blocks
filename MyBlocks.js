8dc0b21f1dd5bb8f3d767e0539aa94d78851bb6e
Sean McCullough <seanwmccullough@gmail.com>
2013-09-22 03:04:14 -0400

    Pranav's making me committ again.

--- MyBlocks.js
+++ MyBlocks.js
@@ -1,8 +1,21 @@
 Parse.initialize("5qbsu1eWpUXnoeb2JnL7ZcxqPXiajZOGsSkyk7fB", "YYOUQ0x2decE180Il4Y5mZGUIzAcWR7VTGsYsZvb");
 
+/*
+	Print the username of the user currently logged on.
+*/
+function setUserName()
+{
+	var name;
+	var currentUser = Parse.User.current();
+	if(currentUser)
+		name = currentUser.get("username");
+	else
+		name = "No one is signed in.";
+	document.getElementById("loginStatus").innerHTML = name;
+}
+
 function createTable()
 {
-	alert("Inside createTable()");
 	var user = Parse.User.current();
 	var blockArray = user.get("Blocks");
 	var numElements = 0;
@@ -17,4 +30,12 @@ function createTable()
 	}
 	tablecontents += "</table>";
 	document.getElementById("blockTable").innerHTML = tablecontents;
+}
+
+function logOut()
+{
+	if(Parse.User.current())
+		Parse.User.logOut();
+	setUserName();
+	window.open("http://researchblocks.parseapp.com/signIn.html","_self");
 }
--- signIn.js
+++ signIn.js
@@ -1,19 +1,5 @@
 Parse.initialize("YvULQpWeWms3tGPYJYeFR37L1YhZus8T7v2joSvz", "X6uoBy1zoOanP51e08f0Cz396TzeqaQk4OVB7Jo2");
 
-/*
-	Print the username of the user currently logged on.
-*/
-function setUserName()
-{
-	var name;
-	var currentUser = Parse.User.current();
-	if(currentUser)
-		name = currentUser.get("username");
-	else
-		name = "No one is signed in.";
-	document.getElementById("loginStatus").innerHTML = name;
-}
-
 function logIn()
 {	
 	var email = document.getElementById("inputEmail").value;
@@ -21,22 +7,13 @@ function logIn()
 	
 	Parse.User.logIn(email, password, {
 		success: function(user){
-		alert("Signed in successfully.");
-		setUserName();
-		//load up splash page
+		window.open("http://reasearchblocks.parseapp.com/MyBlocks.html","_self");
 		},
 		error: function(user, error){
 		alert("Error: "+ error.code + " "+ error.message);
 		}
 	});
 	return false;
-}
-
-function logOut()
-{
-	if(Parse.User.current())
-		Parse.User.logOut();
-	setUserName();
 }
 
 function openSignUp()
