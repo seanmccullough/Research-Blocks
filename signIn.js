efc61e70fc25ad37fc78bf135d61fb15c1f53583
Sean McCullough <seanwmccullough@gmail.com>
2013-09-22 02:40:50 -0400

    Added a string to identify user in signIn

--- signIn.js
+++ signIn.js
@@ -1,15 +1,21 @@
+Parse.initialize("YvULQpWeWms3tGPYJYeFR37L1YhZus8T7v2joSvz", "X6uoBy1zoOanP51e08f0Cz396TzeqaQk4OVB7Jo2");
 
+/*
+	Print the username of the user currently logged on.
+*/
+function setUserName()
+{
+	var name;
+	var currentUser = Parse.User.current();
+	if(currentUser)
+		name = currentUser.get("username");
+	else
+		name = "No one is signed in.";
+	document.getElementById("loginStatus").innerHTML = name;
+}
 
 function logIn()
 {	
-	Parse.initialize("YvULQpWeWms3tGPYJYeFR37L1YhZus8T7v2joSvz", "X6uoBy1zoOanP51e08f0Cz396TzeqaQk4OVB7Jo2");
-	alert("in login()");
-	var currentUser = Parse.User.current();
-	if (currentUser) {
-		alert("already logged");
-	} 
-	else {
-	alert("not logged");
 	var email = document.getElementById("inputEmail").value;
 	var password = document.getElementById("inputPassword").value;
 	
@@ -23,8 +29,16 @@ function logIn()
 		}
 	});
 	
-	}
+	setUserName();
+	
 	return false;
+}
+
+function logOut()
+{
+	if(Parse.User.current())
+		Parse.User.logOut();
+	setUserName();
 }
 
 function openSignUp()
