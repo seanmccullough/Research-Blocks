5311155e6f5c6e92bcf8642d7763914bafbb0cc4
Sean McCullough <seanwmccullough@gmail.com>
2013-09-22 01:45:07 -0400

    Removed some redundant code

--- signUp.js
+++ signUp.js
@@ -1,14 +1,6 @@
 Parse.initialize("YvULQpWeWms3tGPYJYeFR37L1YhZus8T7v2joSvz", "X6uoBy1zoOanP51e08f0Cz396TzeqaQk4OVB7Jo2");
 function signUp()
 {
-	if(Parse.User.current())
-	{
-		alert("someone got here first");
-		Parse.User.logOut();
-	}
-	else
-	{
-	alert("ready to log in");
 	var user = new Parse.User();
 	var email = document.getElementById("inputEmail").value;
 	var password = document.getElementById("inputPassword").value;
@@ -28,7 +20,6 @@ function signUp()
 			alert("Error: " + error.code + " " + error.message);
 		} 
 		});
-	}
 	
 	return false;
 }
